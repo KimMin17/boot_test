a77715994e47f3b5191b27b382a4e9549bfb97ae
Kim Min <kimmin17@sogang.ac.kr>
2023-10-12 00:10:41 +0900

    update upto 3-6

--- src/main/java/com/example/boot_test/question/QuestionService.java
+++ src/main/java/com/example/boot_test/question/QuestionService.java
@@ -1,12 +1,17 @@
 package com.example.boot_test.question;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import com.example.boot_test.DataNotFoundException;
 
 import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,10 +20,6 @@ import lombok.RequiredArgsConstructor;
 public class QuestionService {
     
     private final QuestionRepository questionRepository;
-
-    public List<Question> getList() {
-        return this.questionRepository.findAll();
-    }
 
     public Question getQuestion(Integer id) {
         Optional<Question> question = this.questionRepository.findById(id);
@@ -36,5 +37,12 @@ public class QuestionService {
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         this.questionRepository.save(q);
+    }
+
+    public Page<Question> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
     }
 }
